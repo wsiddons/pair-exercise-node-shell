cbc5e85fff019a16b80bae7fd4e42480d3f61bcc
Corum Gold <goldcorum@gmail.com>
2022-08-03 16:03:55 -0400

    Finish pwd

--- bash.js
+++ bash.js
@@ -1,11 +1,1 @@
-process.stdout.write('prompt > ')
-
-process.stdin.on('data', (data) => {
-    const cmd = data.toString().trim()
-    if (cmd === 'pwd') {
-        process.stdout.write(__dirname)
-    }
-
-    // process.stdout.write('You typed: ' + cmd)
-    process.stdout.write('\nprompt > ')
-})
+const exportFromPwd = require("./pwd");
--- pwd.js
+++ pwd.js
@@ -1,11 +1,12 @@
 process.stdout.write("prompt > ");
 
-process.stdin.on("data", (data) => {
+const pwdFunc = process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
   if (cmd === "pwd") {
     process.stdout.write(__dirname);
   }
-
   // process.stdout.write('You typed: ' + cmd)
   process.stdout.write("\nprompt > ");
 });
+
+module.exports.pwdFunc
