1aa41e6760e72dc1749601267854a2111029a3a0
wsiddons <wsiddons11@gmail.com>
2022-08-03 15:49:33 -0400

    pwd complete! + nodemon

---
+++ bash.js
@@ -0,0 +1,11 @@
+process.stdout.write('prompt > ')
+
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim()
+    if (cmd === 'pwd') {
+        process.stdout.write(__dirname)
+    }
+
+    // process.stdout.write('You typed: ' + cmd)
+    process.stdout.write('\nprompt > ')
+})
